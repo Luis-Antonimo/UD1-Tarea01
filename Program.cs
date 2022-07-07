07f64fe298a766bf1563003203ce8f3004d22f0f
Luis Antónimo <luisantonimo@gmail.com>
2022-07-08 00:20:37 +0200

    Cambios1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA MUNDO!!!");
+            Console.ReadKey(); //Para la ejecución hasta presionar tecla
+        }
+    }
+}
